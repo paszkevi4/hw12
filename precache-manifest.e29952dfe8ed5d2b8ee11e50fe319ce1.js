74f96c61418757ece0acd4a772cbaa3bd3fcdc91
paszkevi4 <paszkevi4@gmail.com>
2020-08-28 15:59:51 +0300

    Updates

---
+++ precache-manifest.e29952dfe8ed5d2b8ee11e50fe319ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04609480365e0b1845d87cf441b0da01",
+    "url": "/hw12/index.html"
+  },
+  {
+    "revision": "5c9c14819df2a53b76f0",
+    "url": "/hw12/static/css/main.4abed3d4.chunk.css"
+  },
+  {
+    "revision": "3bc6dca74f0d42a0cb37",
+    "url": "/hw12/static/js/2.6790d9e5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hw12/static/js/2.6790d9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9c14819df2a53b76f0",
+    "url": "/hw12/static/js/main.c0de14ba.chunk.js"
+  },
+  {
+    "revision": "35d1d37841ec44c89bcb",
+    "url": "/hw12/static/js/runtime-main.aa41bdb2.js"
+  },
+  {
+    "revision": "1e9cd186b02026e9e7543dc7a5e7fd43",
+    "url": "/hw12/static/media/starsky.1e9cd186.png"
+  }
+]);
